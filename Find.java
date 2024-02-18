052025a34864f567f6ba10ea26b0836da738e48a
jessimounika <pallapumouni6@gmail.com>
2024-02-18 18:49:40 +0530

    sprint

---
+++ Find.java
@@ -0,0 +1,21 @@
+package com.Selinium;
+
+public class Find {
+
+	public static void main(String[] args)
+	{
+		int i=20;
+		int j= 10;
+		System.out.println(i);
+		System.out.println(++i);
+		System.out.println(i++);
+		System.out.println(i);
+
+	}
+
+
+
+
+	}
+
+
